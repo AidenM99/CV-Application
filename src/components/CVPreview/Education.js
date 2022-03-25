d97d30fb83eef769167865f47c8cb1f103e134fd
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-03-25 11:51:31 +0000

    Generate key for each list element

--- src/components/CVPreview/Education.js
+++ src/components/CVPreview/Education.js
@@ -1,15 +1,17 @@
+import uniqid from "uniqid";
+
 const Education = ({ education }) => {
   return (
     <section>
       <h2 className="border-bottom border-dark">Education</h2>
       {education.map((educationItem) => (
-        <div>
-          <p>{educationItem.university}</p>
-          <p>{educationItem.city}</p>
-          <p>{educationItem.degree}</p>
-          <p>{educationItem.subject}</p>
-          <p>{educationItem.from}</p>
-          <p>{educationItem.to}</p>
+        <div key={uniqid()}>
+          <p key={uniqid()}>{educationItem.university}</p>
+          <p key={uniqid()}>{educationItem.city}</p>
+          <p key={uniqid()}>{educationItem.degree}</p>
+          <p key={uniqid()}>{educationItem.subject}</p>
+          <p key={uniqid()}>{educationItem.from}</p>
+          <p key={uniqid()}>{educationItem.to}</p>
         </div>
       ))}
     </section>
