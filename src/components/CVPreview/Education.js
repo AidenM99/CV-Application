69be2b6345b9e3e90014465466b5fdff34affe67
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-03-25 11:32:35 +0000

    Map over education react elements when created so that they respond to correct inputs

--- src/components/CVPreview/Education.js
+++ src/components/CVPreview/Education.js
@@ -1,5 +1,19 @@
-const Education = () => {
-  return <h2 className="border-bottom border-dark">Education</h2>;
+const Education = ({ education }) => {
+  return (
+    <section>
+      <h2 className="border-bottom border-dark">Education</h2>
+      {education.map((educationItem) => (
+        <div>
+          <p>{educationItem.university}</p>
+          <p>{educationItem.city}</p>
+          <p>{educationItem.degree}</p>
+          <p>{educationItem.subject}</p>
+          <p>{educationItem.from}</p>
+          <p>{educationItem.to}</p>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Education;
