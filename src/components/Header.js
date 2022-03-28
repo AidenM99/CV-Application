7f8f17cdda0e65b0dc801d604b9e83ac199a618b
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-03-28 12:20:41 +0100

    Adjust padding on header element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="bg-dark">
+    <header className="bg-dark p-4">
       <h1 className="main-heading text-light"> CV Creator</h1>
     </header>
   );
