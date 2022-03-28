614269cf1419a95ede6c7d20917ea66e50122dde
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-03-28 12:25:39 +0100

    Change form element into a react bootstrap component

--- src/components/CVPreview/CVPreview.js
+++ src/components/CVPreview/CVPreview.js
@@ -2,22 +2,22 @@ import Personal from "./Personal";
 import Education from "./Education";
 import Experience from "./Experience";
 import CVPreviewHeader from "./CVPreviewHeader";
+import { Form } from "react-bootstrap";
 
 const CVPreview = ({ currentCV }) => {
   const { personalInfo } = currentCV;
   const { education } = currentCV;
+  const { experience } = currentCV;
 
   return (
-    <div className="col-lg-6">
-      <form className="preview-cv shadow">
-        <CVPreviewHeader personalInfo={personalInfo} />
-        <div className="cv-preview-main">
-          <Personal personalInfo={personalInfo} />
-          <Education education={education} />
-          <Experience />
-        </div>
-      </form>
-    </div>
+    <Form className="preview-cv shadow">
+      <CVPreviewHeader personalInfo={personalInfo} />
+      <div className="cv-preview-main">
+        <Personal personalInfo={personalInfo} />
+        <Education education={education} />
+        <Experience experience={experience} />
+      </div>
+    </Form>
   );
 };
 
