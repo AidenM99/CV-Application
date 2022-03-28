cb77f93ad27eeff0a4b2ef2883a87b3587d0a251
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-03-28 12:21:45 +0100

    Refactor personal section

--- src/components/CVForm/Personal.js
+++ src/components/CVForm/Personal.js
@@ -1,47 +1,12 @@
-import { Form } from "react-bootstrap";
+import PersonalItems from "./PersonalItems";
 
 const Personal = ({ handlePersonalChange }) => {
   return (
     <section className="form-section personal-section">
       <div className="section-heading-container">
         <h3 className="section-heading">Personal</h3>
+        <PersonalItems handlePersonalChange={handlePersonalChange} />
       </div>
-      <Form.Group>
-        <Form.Control
-          className="mb-3"
-          type="text"
-          id="firstName"
-          placeholder="First Name"
-          onChange={(e) => handlePersonalChange(e)}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Control
-          className="mb-3"
-          type="text"
-          id="lastName"
-          placeholder="Last Name"
-          onChange={(e) => handlePersonalChange(e)}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Control
-          className="mb-3"
-          type="text"
-          id="title"
-          placeholder="Title"
-          onChange={(e) => handlePersonalChange(e)}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Control
-          className="mb-3"
-          type="text"
-          id="about"
-          placeholder="About"
-          onChange={(e) => handlePersonalChange(e)}
-        />
-      </Form.Group>
     </section>
   );
 };
