b226c4668f936669920fc3d14739976aa7b66a5b
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-03-29 14:56:18 +0100

    Swap around education and experience sections and change form element to div element

--- src/components/CVPreview/CVPreview.js
+++ src/components/CVPreview/CVPreview.js
@@ -2,7 +2,6 @@ import Personal from "./Personal";
 import Education from "./Education";
 import Experience from "./Experience";
 import CVPreviewHeader from "./CVPreviewHeader";
-import { Form } from "react-bootstrap";
 import React from "react";
 
 const CVPreview = React.forwardRef((props, ref) => {
@@ -11,14 +10,14 @@ const CVPreview = React.forwardRef((props, ref) => {
   const { experience } = props.currentCV;
 
   return (
-    <Form ref={ref} className="preview-cv shadow min-vh-100 position-sticky">
+    <div ref={ref} className="preview-cv shadow min-vh-100 position-sticky">
       <CVPreviewHeader personalInfo={personalInfo} />
       <div className="cv-preview-main pe-5 ps-5 pt-4 pb-4">
         <Personal personalInfo={personalInfo} />
-        <Education education={education} />
         <Experience experience={experience} />
+        <Education education={education} />
       </div>
-    </Form>
+    </div>
   );
 });
 
